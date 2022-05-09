bc68a21d1e58523d50ba9aad42f87b39dab814e0
vikiwagaj <vikiwagaj@gmail.com>
2022-05-09 16:43:13 +0530

    first git project 1

---
+++ src/main/java/ktctc2/GitProject1/App.java
@@ -0,0 +1,13 @@
+package ktctc2.GitProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
