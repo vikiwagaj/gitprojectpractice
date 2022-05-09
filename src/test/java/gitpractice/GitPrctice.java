9dec6280d3c0500909d420a358cf5f9283ae5bc4
vikiwagaj <vikiwagaj@gmail.com>
2022-05-09 16:59:35 +0530

    first updated

---
+++ src/test/java/gitpractice/GitPrctice.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class GitPrctice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("my first commit");
+	}
+
+}
