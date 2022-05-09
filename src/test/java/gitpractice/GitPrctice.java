8ec65cde7c4451915bf111cac016d57437ed98c6
vikiwagaj <vikiwagaj@gmail.com>
2022-05-09 17:11:25 +0530

    updeted by me

--- src/test/java/gitpractice/GitPrctice.java
+++ src/test/java/gitpractice/GitPrctice.java
@@ -5,6 +5,7 @@ public class GitPrctice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("my first commit");
+      System.out.println("my second commit");
 	}
 
 }
